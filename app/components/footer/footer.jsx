0fa8338caf41e36019acf67ffdd26894500119b6
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-15 09:43:50 -0400

    fixed footer

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,10 +1,35 @@
 'use client'
-import { styled } from 'styled-components'
+import styled from 'styled-components'
+import CopyRight from './copyright'
+import QuickLinks from './quicklinks'
+import SocialMedia from './socialmedia'
+import Address from './address'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <Wrapper>
+      <div className='footer-container'>
+        <QuickLinks />
+        <SocialMedia />
+        <Address />
+      </div>
+      <CopyRight />
+    </Wrapper>
+  )
 }
-
-const Wrapper = styled.footer``
-
+const Wrapper = styled.footer`
+  border-top: 2px solid var(--gray-5);
+  a {
+    color: var(--black);
+  }
+  .footer-container {
+    padding: 1rem;
+  }
+  @media (min-width: 992px) {
+    .footer-container {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+`
 export default Footer
---
+++ app/components/footer/quicklinks.jsx
@@ -0,0 +1,48 @@
+import { Button } from 'antd'
+import Link from 'next/link'
+import React from 'react'
+import styled from 'styled-components'
+
+const nav = [
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/about' },
+  { name: 'Modals', link: '/modals' },
+  { name: 'Contact', link: '/contact' },
+  { name: 'Login', link: '/login' },
+  { name: 'Register', link: '/register' },
+]
+const QuickLinks = () => {
+  return (
+    <Wrapper>
+      <p>Quick Links</p>
+      <ul className='container'>
+        {nav.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link href={item.link}>{item.name}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  p {
+    font-size: large;
+    font-weight: 600;
+  }
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  li {
+    padding: 0.3rem 0;
+    :hover {
+      color: var(--primary-5);
+    }
+  }
+`
+
+export default QuickLinks
